c567da3d41b612a01b1131c771aa8cc0ad62762f
yuzushioh <yuzushioh@gmail.com>
2018-01-20 22:12:59 +0900

    add test project and fix market model

---
+++ Example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by yuzushioh on 2018/01/20.
+//  Copyright © 2018 yuzushioh. All rights reserved.
+//
+
+import UIKit
+import BittrexKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
